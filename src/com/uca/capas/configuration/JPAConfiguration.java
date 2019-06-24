22b229679a55ebf19f797ab569b4e081b1cf698e
Carlos Rafael Contreras Rauda <37202907+crcr97@users.noreply.github.com>
2019-06-24 00:08:27 -0600

    BackUp de Base Agregado

--- src/com/uca/capas/configuration/JPAConfiguration.java
+++ src/com/uca/capas/configuration/JPAConfiguration.java
@@ -45,7 +45,7 @@ public class JPAConfiguration {
 		dataSource.setDriverClassName("org.postgresql.Driver");
 		dataSource.setUrl("jdbc:postgresql://127.0.0.1:5432/tareaNCapas");
 		dataSource.setUsername("postgres");
-		dataSource.setPassword("root");
+		dataSource.setPassword("postgres");
 		return dataSource;
 	}
 	
