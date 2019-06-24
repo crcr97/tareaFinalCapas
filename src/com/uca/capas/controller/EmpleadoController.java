f1792a1a1f77c00a5ae0c6fbf1f0666d3570d687
Carlos Rafael Contreras Rauda <37202907+crcr97@users.noreply.github.com>
2019-06-23 21:40:40 -0600

    Arreglando errores

---
+++ src/com/uca/capas/controller/EmpleadoController.java
@@ -0,0 +1,93 @@
+package com.uca.capas.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.uca.capas.dto.EditarEmpDTO;
+import com.uca.capas.service.EmpleadoService;
+import com.uca.capas.service.SucursalService;
+
+@Controller
+public class EmpleadoController {
+
+	@Autowired
+	SucursalService sucursalService;
+	
+	@Autowired
+	EmpleadoService empleadoService;
+	
+	@RequestMapping(value = "/empleado/main", method = {RequestMethod.GET, RequestMethod.POST})
+	public ModelAndView main(@RequestParam(name = "id") Integer id){
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("sucursal", sucursalService.findOne(id));
+		mav.setViewName("empleados/main");
+		return mav;
+	}
+	
+	@RequestMapping(value = "/empleado/eliminar", method = RequestMethod.POST)
+	public ModelAndView eliminarEmpleado(@RequestParam(name = "empId",  required = true) Integer empId, @RequestParam(name = "sucId",  required = true) Integer sucId){
+		ModelAndView mav = new ModelAndView();
+		empleadoService.eliminarEmpleado(empId);
+		mav.addObject("sucursal", sucursalService.findOne(sucId));
+		mav.setViewName("empleados/main");
+		return mav;
+	}
+	
+	@RequestMapping(value = "/empleado/ver", method = RequestMethod.POST)
+	public ModelAndView verEmpleado(@RequestParam(name = "empId2",  required = true) Integer empId, @RequestParam(name = "sucId2",  required = true) Integer sucId){
+		ModelAndView mav = new ModelAndView();
+		//empleadoService.eliminarEmpleado(empId);
+		mav.addObject("editarEmpDTO", empleadoService.editEmpDTO(empId));
+		mav.addObject("IdPadre", sucId);
+		mav.setViewName("empleados/editarEmpleado");
+		return mav;
+	}
+	
+	@RequestMapping(value = "/empleado/editar", method = RequestMethod.POST)
+	public ModelAndView editar(@Valid @ModelAttribute EditarEmpDTO editarDTO, BindingResult result, 
+			@RequestParam(name = "empId") Integer empId){
+		ModelAndView mav = new ModelAndView();
+		if(result.hasErrors()) {
+			mav.addObject("IdPadre", empId);
+			mav.setViewName("empleados/editarEmpleado");
+		}else {
+			empleadoService.editarEmpleado(editarDTO);
+			mav.addObject("sucursal", sucursalService.findOne(empId));
+			mav.setViewName("empleados/main");
+		}
+		return mav;
+	}
+	
+	@RequestMapping(value = "/empleado/nuevo", method = RequestMethod.GET)
+	public ModelAndView registrar(@RequestParam(name = "id") Integer empId){
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("editarEmpDTO", new EditarEmpDTO());
+		//System.out.println(empId);
+		mav.addObject("IDSucursal", empId);
+		mav.setViewName("empleados/registrarEmpleado");
+		return mav;
+	}
+	
+	@RequestMapping(value = "/empleado/registrar", method = RequestMethod.POST)
+	public ModelAndView nuevaSucursal(@Valid @ModelAttribute EditarEmpDTO editarDTO, BindingResult result,
+			@RequestParam(name = "sucursalId", required = true) Integer empId){
+		ModelAndView mav = new ModelAndView();
+		if(result.hasErrors()) {
+			mav.addObject("IDSucursal", empId);
+			mav.setViewName("empleados/registrarEmpleado");
+		}else {
+			empleadoService.agregarEmpleado(editarDTO, empId);
+			mav.addObject("sucursal", sucursalService.findOne(empId));
+			mav.setViewName("empleados/main");
+		}
+		return mav;
+	}
+}
---
+++ src/com/uca/capas/controller/SucursalController.java
@@ -0,0 +1,88 @@
+package com.uca.capas.controller;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.uca.capas.domain.Sucursal;
+import com.uca.capas.dto.EditarDTO;
+import com.uca.capas.service.EmpleadoService;
+import com.uca.capas.service.SucursalService;
+
+@Controller
+public class SucursalController {
+
+	@Autowired
+	SucursalService sucursalService;
+	
+	@Autowired
+	EmpleadoService empleadoService;
+	
+	
+	@RequestMapping(value = "/sucursal/eliminar", method = RequestMethod.POST)
+	public ModelAndView eliminar(@RequestParam(name = "valorId") Integer id){
+		ModelAndView mav = new ModelAndView();
+		sucursalService.eliminarSucursal(id);
+		mav.addObject("sucursales", sucursalService.findAll());
+		mav.setViewName("sucursales/main");
+		return mav;
+	}
+	
+	@RequestMapping(value = "/sucursal/main", method = RequestMethod.GET)
+	public ModelAndView main(){
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("sucursales", sucursalService.findAll());
+		mav.setViewName("sucursales/main");
+		return mav;
+	}
+	
+	@RequestMapping(value = "/sucursal/ver", method = RequestMethod.POST)
+	public ModelAndView ver(@RequestParam(name = "editId") Integer id){
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("editarDTO", sucursalService.obtenerSucursalDTO(id));
+		mav.setViewName("sucursales/edit");
+		return mav;
+	}
+	
+	@RequestMapping(value = "/sucursal/editar", method = RequestMethod.POST)
+	public ModelAndView editar(@Valid @ModelAttribute EditarDTO editarDTO, BindingResult result){
+		ModelAndView mav = new ModelAndView();
+		if(result.hasErrors()) {
+			mav.setViewName("sucursales/edit");
+		}else {
+			sucursalService.editarSucursal(editarDTO);
+			mav.setViewName("sucursales/main");
+		}
+		return mav;
+	}
+	
+	@RequestMapping(value = "/sucursal/nueva", method = RequestMethod.GET)
+	public ModelAndView registrar(){
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("editarDTO", new EditarDTO());
+		mav.setViewName("sucursales/registrar");
+		return mav;
+	}
+
+	@RequestMapping(value = "/sucursal/registrar", method = RequestMethod.POST)
+	public ModelAndView nuevaSucursal(@Valid @ModelAttribute EditarDTO editarDTO, BindingResult result){
+		ModelAndView mav = new ModelAndView();
+		if(result.hasErrors()) {
+			mav.setViewName("sucursales/registrar");
+		}else {
+			sucursalService.agregarSucursal(editarDTO);
+			mav.setViewName("sucursales/main");
+		}
+		return mav;
+	}
+	
+}
